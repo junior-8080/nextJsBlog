80f82ec119a2629157d408062eeabe29e7265a4a
junior-8080 <aabdulmukhsin@yahoo.com>
2021-03-10 16:43:23 +0000

    testing

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import articleStyles from '../styles/Article.module.css'
 
 const ArticleItem = ({article}) => {
+    
 
     return (
        
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
-import {useState,useEffect} from 'react';
 import Head from 'next/head'
 import ArticleList from '../components/ArticleList'
 // import styles from '../styles/Home.module.css'
 
 
 function Home({articles}) {
-    
-     const [activeArticles,setActive] = useState([]);
-     const [page,setPage] = useState(3);
-    const handleClick = () => {
-     
-      if(activeArticles.length !== articles){
-        setActive(articles.slice(0,page + 3))
-      }
-      setPage(page + 3)
-    }
-    useEffect(() => {
-       
-     setActive(articles.slice(0,3)) 
-    
-    },[]);
+ 
 
     return (
         <div>
@@ -27,10 +12,7 @@ function Home({articles}) {
                 <title>WebDev News</title>
                 <meta name="keyword" content='web development, programming'></meta>
             </Head>
-            <ArticleList  articles = {activeArticles}/>
-            <div style = {{display:"flex",justifyContent:"center",marginTop:'5em'}}>
-              {activeArticles.length < articles.length ? <button onClick={handleClick}>Load More</button>: ''}
-            </div>
+            <ArticleList  articles = {articles}/>
         </div>
     )
 
